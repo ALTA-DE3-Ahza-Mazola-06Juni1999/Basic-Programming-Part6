29e0a61eafe2a4c09dd87b471dd7288e63889dcd
Ahza Mazola <ahzamazola@gmail.com>
2024-05-12 23:00:10 +0700

    output masih salah

--- problem3/main.py
+++ problem3/main.py
@@ -1,6 +1,15 @@
 def array_unique(arrayA, arrayB):
-    # your code here
-    return ''
+    unique_arr= []
+
+    for item in arrayA:
+        if item not in arrayB:
+            unique_arr.append(item)
+
+    for item in arrayB:
+        if item not in arrayA and item not in unique_arr:
+            unique_arr.append(item)
+
+    return unique_arr
 
 if __name__ == '__main__':
     print(array_unique([1, 2, 3, 4], [1, 3, 5, 10, 16])) # [2, 4]
