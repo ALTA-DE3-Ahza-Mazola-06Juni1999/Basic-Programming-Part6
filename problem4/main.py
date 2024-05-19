b6767d8c46fbe05c34730fc86cc5fe552da8ba93
Ahza Mazola <ahzamazola@gmail.com>
2024-05-19 16:39:06 +0700

    add answer

--- problem4/main.py
+++ problem4/main.py
@@ -1,5 +1,16 @@
 def find_max_sum_sub_array(k, arr):
-    return 0
+    start = 0 
+    current_sum = 0
+    max_sum = float('-inf')
+
+    for end in range(len(arr)):
+        current_sum += arr[end]
+
+        if end >= k-1:
+            max_sum = max(max_sum, current_sum)
+            current_sum -= arr[start]
+            start += 1
+    return max_sum
 
 if __name__ == '__main__':
     print(find_max_sum_sub_array(3, [2, 1, 5, 1, 3, 2])) # 9
