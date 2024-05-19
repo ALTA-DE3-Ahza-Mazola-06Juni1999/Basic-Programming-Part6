c49ed86100455e73bdc8bdec8f4a2a5ac98b7af1
Ahza Mazola <ahzamazola@gmail.com>
2024-05-19 16:15:01 +0700

    add answer and switching import and from caesar in unittest

--- problem2/main.py
+++ problem2/main.py
@@ -1,5 +1,17 @@
 def caesar(offset, input_str):
-    return ''
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    result = ""
+    
+    for i in input_str:
+        if i.isalpha():
+            is_upper = i.isupper()
+            i = i.lower()
+            shifted_index =(alphabet.index(i) + offset) % 26
+            shifted_i = alphabet[shifted_index]
+            result += shifted_i.upper()if is_upper else shifted_i
+        else:
+            result += char
+    return result
 
 if __name__ == '__main__':
     print(caesar(3, "abc")) # def
