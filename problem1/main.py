72e463217ce5bd35f9bd6df239318e59f34b29b9
Ahza Mazola <ahzamazola@gmail.com>
2024-05-19 15:58:35 +0700

    add answer

--- problem1/main.py
+++ problem1/main.py
@@ -1,5 +1,9 @@
 def compare(a, b):
     pattern = ""
+    for i in range(len(a)):
+        for k in range(i + 1, len(a) + 1):
+            if a[i:k] in b and len(a[i:k]) > len(pattern):
+                pattern = a[i:k] 
     return pattern
 
 if __name__ == '__main__':
